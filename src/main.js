04ed04a278484b51d0e6c32eb62c7772f2531931
tamsalanghae <tamsalanghae@github.com>
2020-12-15 20:17:46 +0700

    init vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faUserSecret} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import VueRouter from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+// Install BootstrapVue
+Vue.use(BootstrapVue).use(IconsPlugin).use(Vuex).use(VueRouter)
+// // Optionally install the BootstrapVue icon components plugin
+// Vue.use(IconsPlugin)
+// Vue.use(Vuex)
+// Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
